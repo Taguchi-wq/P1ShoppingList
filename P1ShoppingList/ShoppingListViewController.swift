79cdcc5b0158a916e3b20cd626a551d6e4af514b
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-02 23:03:49 +0900

    first commit

---
+++ P1ShoppingList/ShoppingListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShoppingListViewController.swift
+//  P1ShoppingList
+//
+//  Created by cmStudent on 2021/07/02.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
