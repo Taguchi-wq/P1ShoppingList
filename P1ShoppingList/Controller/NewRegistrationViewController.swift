f1640895368a041616dea4972974881a804de6de
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-03 00:07:09 +0900

    新規登録画面を作成

---
+++ P1ShoppingList/Controller/NewRegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewRegistrationViewController.swift
+//  P1ShoppingList
+//
+//  Created by cmStudent on 2021/07/03.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class NewRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
