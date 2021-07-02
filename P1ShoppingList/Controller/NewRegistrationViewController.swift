40103c073a5ee0a8d61ba34dfc494e3a7faebe1a
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-03 00:42:34 +0900

    thingCellをshoppingHistoryListTableViewに配置

--- P1ShoppingList/Controller/NewRegistrationViewController.swift
+++ P1ShoppingList/Controller/NewRegistrationViewController.swift
@@ -10,18 +10,44 @@ import UIKit
 
 class NewRegistrationViewController: UIViewController {
 
+    @IBOutlet private weak var shoppingHistoryListTableView: UITableView!
     @IBOutlet private weak var textField: UITextField!
     @IBOutlet private weak var addThingButton: UIButton!
+    
+    
+    private let reuseIdentifier = "ThingCell"
+    private let things: [String] = ["トイレットペーパー", "マヨネーズ", "カレー"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupShoppingHistoryListTableView()
         setupAddThingButton()
+    }
+    
+    private func setupShoppingHistoryListTableView() {
+        shoppingHistoryListTableView.dataSource = self
+        shoppingHistoryListTableView.register(UINib(nibName: reuseIdentifier, bundle: nil),
+                                             forCellReuseIdentifier: reuseIdentifier)
     }
     
     private func setupAddThingButton() {
         addThingButton.layer.cornerRadius = addThingButton.bounds.height / 2
     }
 
+}
+
+extension NewRegistrationViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return things.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let thingCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ThingCell
+        thingCell.setupThingCell(thing: things[indexPath.row])
+        return thingCell
+    }
+    
 }
