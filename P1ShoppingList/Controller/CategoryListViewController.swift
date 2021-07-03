4902c6d14fa0a1590edde631547d03cea1ab0b99
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-03 12:31:43 +0900

    カテゴリー画面の作成

---
+++ P1ShoppingList/Controller/CategoryListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryListViewController.swift
+//  P1ShoppingList
+//
+//  Created by cmStudent on 2021/07/03.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class CategoryListViewController: UIViewController {
+
+    @IBOutlet private weak var categoryListTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
