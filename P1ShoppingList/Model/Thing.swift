14764b37474e535b73f83313e708aa707aad5707
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-03 13:25:50 +0900

    Thingモデルを作成

---
+++ P1ShoppingList/Model/Thing.swift
@@ -0,0 +1,17 @@
+//
+//  Thing.swift
+//  P1ShoppingList
+//
+//  Created by cmStudent on 2021/07/03.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Thing: Object {
+    @objc dynamic var categoryID = Int()
+    @objc dynamic var createdAt  = Date()
+    @objc dynamic var thingName  = String()
+    @objc dynamic var deleteFlag = Bool()
+}
